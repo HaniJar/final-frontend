ade2d3d15a5d742bf427f3989df13a007cf828fb
Haniah Jardien <jardienhaniah@gmail.com>
2022-03-11 14:58:11 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,75 @@
+<template>
+  <MDBNavbar expand="lg" light bg="light" container>
+    <MDBNavbarToggler
+      target="#navbarCenterAlignExample"
+      @click="collapse6 = !collapse6"
+    ></MDBNavbarToggler>
+    <MDBCollapse id="navbarCenterAlignExample" v-model="collapse6">
+      <MDBNavbarNav center class="mb-2 mb-lg-0">
+        <MDBNavbarItem router-link to="/">Home</MDBNavbarItem>
+        <MDBNavbarItem router-link to="/About">About</MDBNavbarItem>
+        <MDBNavbarItem router-link to="/Login">Login</MDBNavbarItem>
+        <MDBNavbarItem router-link to="/Register">Register</MDBNavbarItem>
+        <MDBNavbarItem router-link to="/Products">Products</MDBNavbarItem>
+        <MDBNavbarItem router-link to="/Cart">Cart</MDBNavbarItem>
+        <MDBNavbarItem router-link to="/Profile">Profile</MDBNavbarItem>
+        <MDBNavbarItem router-link to="/Admin">Admin</MDBNavbarItem>
+        <MDBNavbarItem router-link to="/Contact">Contact</MDBNavbarItem>
+
+        <button class="btn" v-if="currentUser" @click="logOut">Logout</button>
+      </MDBNavbarNav>
+    </MDBCollapse>
+  </MDBNavbar>
+</template>
+
+<script>
+import {
+  MDBNavbar,
+  MDBNavbarToggler,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBCollapse,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+} from "mdb-vue-ui-kit";
+import { ref } from "vue";
+export default {
+  components: {
+    MDBNavbar,
+    MDBNavbarToggler,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBCollapse,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+  },
+  setup() {
+    const collapse6 = ref(false);
+    const dropdown9 = ref(false);
+    return {
+      collapse6,
+      dropdown9,
+    };
+  },
+  // methods: {
+  //   toggleNav() {
+  //     this.isActive = !this.isActive;
+  //   },
+  //   logOut() {
+  //     this.$store.dispatch("auth/logout");
+  //     this.$router.push("/login");
+  //   },
+  // },
+  // computed: {
+  //   currentUser() {
+  //     return this.$store.state.auth.user;
+  //   },
+  // },
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,67 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+import Products from "@/views/Products.vue";
+import Cart from "@/views/Cart.vue";
+import Profile from "@/views/Profile.vue";
+import Admin from "@/views/Admin.vue";
+import Contact from "@/views/Contact.vue";
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: About,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/products",
+    name: "Products",
+    component: Products,
+  },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: Cart,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+  },
+  {
+    path: "/admin",
+    name: "Admin",
+    component: Admin,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
